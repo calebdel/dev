de19902ccb904918f923c147ee26028f08ecc8f2
Gavin Dunne <g@veen.ca>
2015-10-08 15:08:34 -0700

    make better the fish config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,26 +1,32 @@
+set    fish_greeting ""
+set -x PATH $HOME/bin $PATH
+
 # BYOBU (auto-launch)
+if status --is-login; byobu-launcher
+else if status --is-interactive
 
-  test -n "$BYOBU_WINDOW_NAME" -o -z "$TERM"; or byobu-launcher
+  # DIRENV
+    eval (direnv hook fish)
 
-# DIRENV
-
-  eval (direnv hook fish)
-
-# CHRUBY
-
-  source /usr/local/share/chruby/chruby.fish
-
-  function ruby-install
-    /usr/local/bin/ruby-install --src-dir /tmp --rubies-dir /opt/rubies --cleanup $argv
+  # CHRUBY
     source /usr/local/share/chruby/chruby.fish
-    chruby $argv
-    gem install bundler
-  end
+    chruby ruby ^ /dev/null
 
-# NVM
+    function ruby-install
+      /usr/local/bin/ruby-install --src-dir /tmp --rubies-dir /opt/rubies --cleanup $argv
+      source /usr/local/share/chruby/chruby.fish
+      chruby $argv
+      gem install bundler
+    end
 
-  function nvm
-    bass source /opt/nvm/nvm.sh ';' nvm $argv
-  end
+  # NVM
+    function nvm
+      bass source /opt/nvm/nvm.sh ';' nvm $argv
+    end
 
-  test (nvm current) = "none"; and nvm use  --delete-prefix stable
+    nvm use stable > /dev/null ^ /dev/null
+
+  # STATUS MESSAGE
+  dev-status
+
+end
