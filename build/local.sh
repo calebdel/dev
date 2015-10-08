4aac1ba7ba9ce0b104ab46b9711902817457901a
Gavin Dunne <g@veen.ca>
2015-10-08 15:02:47 -0700

    remove sudo from root run script

--- build/local.sh
+++ build/local.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sudo apt-add-repository -y ppa:fish-shell/release-2
-sudo apt-get update
-sudo apt-get install -y fish
+apt-add-repository -y ppa:fish-shell/release-2
+apt-get update
+apt-get install -y fish
 
 curl -sSL https://github.com/JeanMertz/chruby-fish/archive/v0.6.0.tar.gz | tar -zxvf -
 cd chruby-fish-0.6.0 && make install
